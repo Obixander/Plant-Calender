3c499b3df255f4c433e73750ca10d9f9859e95bd
obixander <noahhauge@gmail.com>
2025-03-24 11:58:40 +0100

    Added repositories and interfaces for the repositories

---
+++ DataAccess/Repositories/PlantPlotHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.I;
+using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PlantPlotHistoryRepository : GenericRepository,IPlantPlotHistoryRepository
+    {
+    }
+}
---
+++ DataAccess/Repositories/PlantRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PlantRepository : GenericRepository,IPlantRepository
+    {
+
+    }
+}
